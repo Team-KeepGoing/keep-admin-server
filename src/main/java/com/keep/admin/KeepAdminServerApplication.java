a0922c9f0f50d5292193112aa217a94829c92495
최미래 <miraechoi@dgsw.hs.kr>
2025-03-27 17:35:23 +0900

    Initial Commit

---
+++ src/main/java/com/keep/admin/KeepAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.keep.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepAdminServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keep/admin/KeepAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keep.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
